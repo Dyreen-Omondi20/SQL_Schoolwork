e60c14d008483cb26ea89282f0a8bee3a6626d0c
Dyreen Omondi <dyreenomondi20@gmail.com>
2025-07-18 17:41:25 +0300

    insert.sql

---
+++ insert.sql
@@ -0,0 +1,16 @@
+INSERT INTO access_to_basic_services (
+Region,
+Sub_region,
+Country_name,
+Time_period,
+Pct_managed_drinking_water_services,
+Pct_managed_sanitation_services,
+Est_population_in_millions,
+Est_gdp_in_billions,
+Land_area,
+Pct_unemployment,
+Gini_index
+)
+VALUES (
+  'Sub-Saharan Africa', 'Southern Africa', 'South Africa', 2020, 92.0, 78.67,58.801927, 337.62, 1213090.0, 24.34, 0
+  );

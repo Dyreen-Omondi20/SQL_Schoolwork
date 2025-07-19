b62814edabd1d96134d086c77780c29b35262289
Dyreen Omondi <dyreenomondi20@gmail.com>
2025-07-19 12:13:08 +0300

    Checking content available in Maji Ndogo Database

---
+++ SELECT AND LIMIT.sql
@@ -0,0 +1,19 @@
+SELECT *
+FROM location
+WHERE province_name= "Akatsi" 
+AND town_name= "Harare"
+LIMIT 5;
+
+SELECT*
+FROM md_water_services.visits
+LIMIT 5;
+
+SELECT *
+FROM water_source
+LIMIT 5;
+
+
+
+
+
+
